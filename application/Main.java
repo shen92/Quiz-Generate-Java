372212bd3c8d60632f6bc8aee0f3b4b51debcd63
Yingjie Shen <shen92@wisc.edu>
2019-04-16 20:20:47 -0500

    沈英杰 2019-04-16 20:20:41 测试2

--- application/Main.java
+++ application/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
   public void start(Stage primaryStage) {
     try {
       System.out.println("Hello World!");
+      System.out.println("!!!");
       BorderPane root = new BorderPane();
       Scene scene = new Scene(root, 400, 400);
       scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
