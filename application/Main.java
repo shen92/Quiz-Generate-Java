d27e5a0f3fcf2a88489c725b68ef778b57bca239
Yingjie Shen <shen92@wisc.edu>
2019-04-16 20:35:09 -0500

    Yingjie Shen 2019-04-16 20:35:01 Test4

--- application/Main.java
+++ application/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     try {
       System.out.println("Hello World!");
       System.out.println("Do you like it?");
+      System.out.println("Yes♂");
       BorderPane root = new BorderPane();
       Scene scene = new Scene(root, 400, 400);
       scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
