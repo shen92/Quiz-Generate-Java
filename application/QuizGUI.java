c71c945abc20ac4250da987de194a8a0ab879a83
Zhelai Chen <zchen743@wisc.edu>
2019-04-23 23:28:42 -0500

    Zhelai Chen 11:28 pm 23/04

--- application/QuizGUI.java
+++ application/QuizGUI.java
@@ -30,27 +30,16 @@ public class QuizGUI extends Application {
 
 
     try {
-      // Text t = new Text("123");
+      //Title: Question 01/20
       Text questionLabel = new Text("Question " + indexOfQuestion + "/" + totalNumOfQuestion);
       questionLabel.setFont(Font.font("Arial", FontWeight.BOLD, 40));
-      // questionLabel.
-
-      // VBox titleVBox = new VBox();
       HBox titleHBox = new HBox();
       titleHBox.setPrefHeight(100);
       titleHBox.setPrefWidth(1200);
       titleHBox.setAlignment(Pos.CENTER);
-
       titleHBox.getChildren().add(questionLabel);
-      // titleVBox.setPadding(new Insets(25.0, 400.0, 40.0, 400.0));
-      // titleHBox.getChildren().add(titleHBox);
-
-
-      // questionLabel.setAlignment(Pos.CENTER);
-
-      // Label indexOfQuestionLabel = new Label(indexOfQuestion + "");
-      // indexOfQuestionLabel.setTranslateY(50);
-      // Label totalNumOfQuestionLabel = new Label(totalNumOfQuestion + "");
+      
+      //get question from the question list.
       Text questionsText = new Text("" + "QuestionClass.getQuestion()");
       questionsText.setFont(Font.font("Arial", 20));
       VBox questionVBox = new VBox();
@@ -59,40 +48,27 @@ public class QuizGUI extends Application {
       questionVBox.setPadding(new Insets(25.0, 30.0, 40.0, 150.0));
       questionVBox.getChildren().add(questionsText);
 
-      // questionVBox.getChildren().add(questionsText);
-
-
-      ToggleGroup group = new ToggleGroup();
-
-
-
+    
+      //choises: use for loop to get the choices
       VBox VBoxAnswers = new VBox();
       VBoxAnswers.setPadding(new Insets(25.0, 40.0, 40.0, 150.0));
       VBoxAnswers.setSpacing(40);
       VBoxAnswers.setPrefWidth(1200);
       VBoxAnswers.setPrefHeight(600);
-
-
+      ToggleGroup group = new ToggleGroup();
+      //total number of choices
       int size = 3;
       for (int i = 0; i < size; i++) {
         RadioButton button = new RadioButton("QuestionClass.getAnswers");
         button.setToggleGroup(group);
         button.setSelected(true);
         VBoxAnswers.getChildren().add(button);
-        // VBoxAnswers.setAlignment(Pos.CENTER);
       }
 
-
-
-      // HBoxAnswers.getChildren().add(questionsText);
-
-      // HBoxQuestion.getPadding();
-
+     
+      //pre and next button
       Button toPreviousButton = new Button("Previous");
       Button toNextButton = new Button("Next");
-      // toPreviousButton.setAlignment(Pos.BOTTOM_LEFT);
-      // toNextButton.setAlignment(Pos.BOTTOM_RIGHT);
-
       HBox buttonHBox = new HBox();
       buttonHBox.setPadding(new Insets(25, 0, 25, 300));
       buttonHBox.setPrefWidth(1200);
@@ -100,31 +76,12 @@ public class QuizGUI extends Application {
       buttonHBox.setSpacing(300);
       buttonHBox.getChildren().add(toPreviousButton);
       buttonHBox.getChildren().add(toNextButton);
-
-
-      // toPreviousButton.setOnAction( e -> buttonAction());
-
-      // questionLabel.setStyle("-fx-border-color: blue;");
-      // BorderPane canvas = new BorderPane();
-      // canvas.setPadding(new Insets(25.0, 40.0, 40.0, 25.0));
-
-
+     
       VBox root = new VBox();
       root.getChildren().add(titleHBox);
       root.getChildren().add(questionVBox);
       root.getChildren().add(VBoxAnswers);
       root.getChildren().add(buttonHBox);
-
-      // root.setRight(toNextButton);
-      // root.setLeft(toPreviousButton);
-      // root.setBottom(toNextButton);
-      // root.setTop(indexOfQuestionLabel);
-      // root.setTop(totalNumOfQuestionLabel);
-
-      // root.getChildren().add(root);
-      // root.getChildren().add(toPreviousButton);
-      // root.getChildren().add(toNextButton);
-
       Scene scene = new Scene(root, 1200, 800);
       scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
       primaryStage.setScene(scene);
