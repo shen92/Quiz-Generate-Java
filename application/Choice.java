0eeacd5e094bb748ecf5100278ab9e2e9d3c8ace
Yingjie Shen <shen92@wisc.edu>
2019-04-29 22:18:05 -0500

    Yingjie 2019-04-29 22:17:43 Choice BUG FIX required

---
+++ application/Choice.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.LinkedHashMap;
+import javafx.scene.control.ToggleGroup;
+
+
+public class Choice {
+  private LinkedHashMap<String, String> answer;
+
+  ToggleGroup group = new ToggleGroup();
+
+  public Choice() {
+
+  }
+}
